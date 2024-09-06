dcfdb874c1a9edb20088d0d5bd04d9fb7a841b34
Nate <39776302+nloum@users.noreply.github.com>
2024-09-06 18:01:52 -0500

    Improve log messages

--- CodegenBot.Builder/run.sh
+++ CodegenBot.Builder/run.sh
@@ -6,11 +6,14 @@ if [ ! -f /src/bot.json ]; then
 fi
 
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-echo "[$TIMESTAMP] [INFORMATION] [codegen.bot] Copying bot source"
+echo "[$TIMESTAMP] [INFORMATION] [codegen.bot] Copying bot source into container so that the build doesn't produce intermediate artifacts that confuse IDEs. We're explicitly specifying wasi-wasm as the runtime identifier, so that it doesn't have to be specified in the csproj file, so IDEs that don't support wasi-wasm will still show Intellisense."
 cp /src /tmp-src -r
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-echo "[$TIMESTAMP] [INFORMATION] [codegen.bot] Building"
+echo "[$TIMESTAMP] [INFORMATION] [codegen.bot] Building the bot"
 dotnet build /tmp-src -c Release -r wasi-wasm
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-echo "[$TIMESTAMP] [INFORMATION] [codegen.bot] Copying built wasm file to bin/Release/net8.0/wasi-wasm/AppBundle/"
+echo "[$TIMESTAMP] [INFORMATION] [codegen.bot] Copying built wasm file to bin/Release/net8.0/wasi-wasm/AppBundle/, which should be outside the container"
+if [ ! -d /src/bin/Release/net8.0/wasi-wasm/AppBundle/ ]; then
+  mkdir -p /src/bin/Release/net8.0/wasi-wasm/AppBundle/
+fi
 cp /tmp-src/bin/Release/net8.0/wasi-wasm/AppBundle/*.wasm /src/bin/Release/net8.0/wasi-wasm/AppBundle/
