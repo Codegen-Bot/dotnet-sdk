99c10934772081feced82c86081d4f147be092fb
Nate <nate@loumtechnologies.com>
2024-10-14 18:11:57 -0500

    Add an easy way to denest objects, useful for graphql servers

---
+++ CodegenBot/DenestExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace CodegenBot;
+
+public class Denested<TItem>
+{
+    public int Depth { get; init; }
+    public required TItem Item { get; init; }
+}
+
+public static class DenestExtensions
+{
+    public static IEnumerable<Denested<TItem>> Denest<TItem>(IEnumerable<TItem> items, Func<TItem, IEnumerable<TItem>> children)
+    {
+        return DenestInternal(items, children, 0);
+    }
+
+    private static IEnumerable<Denested<TItem>> DenestInternal<TItem>(IEnumerable<TItem> items, Func<TItem, IEnumerable<TItem>> children, int depth)
+    {
+        foreach (var item in items)
+        {
+            yield return new Denested<TItem> { Depth = depth, Item = item };
+            var childItems = children(item);
+            if (childItems != null)
+            {
+                foreach (var denestedChild in DenestInternal(childItems, children, depth + 1))
+                {
+                    yield return denestedChild;
+                }
+            }
+        }
+    }
+}
